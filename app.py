67d42aca7b190de78db17b822eb00fab7800cf6a
Adam Adebowale <adebowaleadam0@gmail.com>
2022-09-19 12:12:42 +0100

    auth

--- app.py
+++ app.py
@@ -5,7 +5,7 @@ if __name__ == '__main__':
     app = create_app()
     app.run(debug=True)
 
-#
+
 # @app.route('/')
 # def hello_world():  # put application's code here
 #     return 'Hello World!'
